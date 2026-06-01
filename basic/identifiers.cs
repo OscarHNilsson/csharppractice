2553ef36ae889a6bd5e95f5fdc05d6b8a8a06575
OscarHNilsson <oscarhedbergn@gmail.com>
2026-06-01 19:47:32 +0200

    Exploring identifiers

---
+++ basic/identifiers.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Identifiers
+{
+    static void Main(string[] args)
+    {
+        int a = 10;
+        int b = 20;
+
+        int c;
+
+        c = a+b;
+
+        //How does this work?
+        Console.WriteLine("The sum of two numbers is: {0}", c);
+
+    }
+}
